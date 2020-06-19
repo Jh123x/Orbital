fcdbaaa5e68197d65eaef1b71c4a5c7b9a2d7f42
Junhua <jhsibeizai@gmail.com>
2020-06-20 01:21:19 +0800

    Updated Screens
    Changelog:
    1. Using latest version of screens from space invaders

--- gym_invaders/gym_game/envs/classes/Game/Screens/PlayScreen.py
+++ gym_invaders/gym_game/envs/classes/Game/Screens/PlayScreen.py
@@ -36,6 +36,7 @@ class PlayScreen(Screen):
         self.sensitivity = sensitivity
         self.fps = max_fps
         self.difficulty = difficulty
+        self.over = False
 
         #Create the groups
         #Bullets shot by player
@@ -124,13 +125,8 @@ class PlayScreen(Screen):
             #Make the enemy shoot
             enemy.shoot()
 
-    def get_enemies(self) -> tuple:
-        """Get tuple of all the enemy sprites"""
-        return tuple(self.enemies)
-
-    def draw_hitboxes(self):
+    def draw_hitboxes(self) -> None:
         """Draw hitboxes for players and objects"""
-
         #Draw hitbox for the enemies
         for sprite in self.enemies:
             pygame.draw.rect(self.surface, (5,55,0), sprite.rect, 0)
@@ -301,6 +297,10 @@ class PlayScreen(Screen):
     def enemy_touched_bottom(self) -> bool:
         """Check if any enemies have touched the bottom of the screen"""
         return len(tuple(filter(lambda x: x.get_y() + x.get_height()//2 > self.screen_height - self.player.get_height(), self.enemies ))) > 0
+
+    def is_over(self) -> bool:
+        """Checks if the game is over"""
+        return self.over
         
     def handle(self) -> State:
         """Handle the drawing of the play state
@@ -311,6 +311,10 @@ class PlayScreen(Screen):
         """
         #If player is destroyed, go to gameover state
         if self.player.is_destroyed():
+            #Set the game to be over
+            self.over = True
+
+            #Return the gameover state
             return State.GAMEOVER
 
         #Check if any of the enemies touched the bottom of the screen
@@ -319,6 +323,9 @@ class PlayScreen(Screen):
             #If it is debugging mode, print out what happened
             if self.debug:
                 print("Alienship hit the player")
+
+            #Set the game to be over
+            self.over = True
                 
             #If so it is gameover for the player
             return State.GAMEOVER
