3515e49c405c1d8366e5d3f42314de68822746f4
blackonyyx <35429757+blackonyyx@users.noreply.github.com>
2020-06-23 20:10:30 +0800

    commit local implementation of policy network and ppo

--- gym_invaders/ai_invader/model/__init__.py
+++ gym_invaders/ai_invader/model/__init__.py
@@ -1,1 +1,2 @@
 from .dqncnn import DQNCNN
+from .basemodel import BaseModel
--- gym_invaders/ai_invader/model/dqncnn.py
+++ gym_invaders/ai_invader/model/dqncnn.py
@@ -1,28 +1,15 @@
-import torch
 import torch.nn as nn
-import torch.autograd as autograd
+from .basemodel import BaseModel
 
-class DQNCNN(nn.Module):
+class DQNCNN(BaseModel):
     '''
     DQN CNN Model to pass frames through for feature detection
     '''
     def __init__(self, input_shape, num_actions):
-        super(DQNCNN, self).__init__()
-        self.input_shape = input_shape
-        self.num_actions = num_actions
-
-        self.features = nn.Sequential(
-            nn.Conv2d(input_shape[0], 32, kernel_size=8, stride=4),
-            nn.ReLU(),
-            nn.Conv2d(32, 64, kernel_size=4, stride=2),
-            nn.ReLU(),
-            nn.Conv2d(64, 64, kernel_size=3, stride=1),
-            nn.ReLU()
-        )
-
+        super(DQNCNN, self).__init__(input_shape,num_actions )
         self.fc = nn.Sequential(
             nn.Linear(self.feature_size(), 512),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.Linear(512, self.num_actions)
         )
 
@@ -33,5 +20,3 @@ class DQNCNN(nn.Module):
         x = self.fc(x)
         return x
 
-    def feature_size(self):
-        return self.features(autograd.Variable(torch.zeros(1, *self.input_shape))).view(1, -1).size(1)
